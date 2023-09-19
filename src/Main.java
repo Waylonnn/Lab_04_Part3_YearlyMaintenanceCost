d5837ec0a794bbb18963fd75a754a33464b6207d
Waylon <weese.619547@swl.k12.oh.us>
2023-09-19 09:17:06 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double winter;
+        double spring;
+        double fall;
+        double summer;
+        double total;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your Maintenance Cost for each Season. \n Winter: ");
+        winter = scan.nextInt();
+        System.out.println("\n Spring: ");
+        spring = scan.nextInt();
+        System.out.println("\n Summer: ");
+        summer = scan.nextInt();
+        System.out.println("\n Fall: ");
+        fall = scan.nextInt();
+
+        total = winter + spring + summer + fall;
+
+        System.out.println("Total Yearly Maintenance Cost: $" + total);
     }
 }
